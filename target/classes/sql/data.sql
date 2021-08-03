3a7e219fc3ef15e4adb26a1feea87d55230e7bd9
Gustavo J <desarrolloglj@gmail.com>
2021-08-02 23:40:25 -0300

    Delete target directory

--- target/classes/sql/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO ITEMS(id, name, long_Description, amount) VALUES
-(1, 'Iphone', 'An Apple Phone', 1000),
-(2, 'TV LG', 'LED Slim TV', 2300),
-(3, 'MacBook Pro', 'An Apple Laptop', 2586),
-(4, 'Mouse', 'Pretty basic mouse', 20),
-(5, 'Keyboard', 'Classic keyboard. With numpad' , 15);
